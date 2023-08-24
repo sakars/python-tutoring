e910f6ad6f5790a8c34ef71ca3b6ba9ffdb6e48c
oputans <o.putaans@gmail.com>
2023-08-25 01:10:49 +0300

    edited 2.6. to include elif and else

--- 2/6_if.py
+++ 2/6_if.py
@@ -1,8 +1,14 @@
-# Ask for the user's year of birth and tell them their age, say "You are at least 18 years old" if they are at least 18 years old
-
-
-
 # Read name from the user, say "Name must be at least 3 characters long" if it is less than 3 characters long
+
+
+
+# Ask for the user's year of birth and tell them their age, 
+# say "You are more than 18 years old" if they are more than 18 years old,
+# say "You are less than 18 years old" if they are less than 18 years old,
+# say "You are 18 years old" if they are 18 years old
+
+
+
 
 
 
